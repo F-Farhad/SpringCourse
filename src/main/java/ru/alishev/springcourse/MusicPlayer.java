6a38534a07d44c944c35a791f32cbd29eb0bc7d0
Fataev Farhod <fataev_f@mail.ru>
2020-10-14 22:54:25 +0300

    All Drop:(

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,7 +1,14 @@
 package ru.alishev.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     private String name;
     private int volume;
@@ -22,16 +29,9 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public void setMusic(Music music){
-        this.music = music;
-    }
-
-    public MusicPlayer() {}
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
-
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for(Music music: musicList){
+            System.out.println(music.getSong());
+        }
     }
 }
