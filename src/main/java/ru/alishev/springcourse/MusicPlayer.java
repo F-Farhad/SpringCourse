ec74383226e38c81a549867384827692b6295152
Fataev Farhod <fataev_f@mail.ru>
2020-10-15 18:37:34 +0300

    HomeWork (DI - list)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,43 +1,18 @@
 package ru.alishev.springcourse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicPlayer {
-    private Music classicalMusic;
-    private Music rockMusic;
+    private List<Music> musicList = new ArrayList<>();
 
-    @Value("${musicPlayer.name}")
-    private String name;
-
-    @Value("${musicPlayer.volume}")
-    private int volume;
-
-    public MusicPlayer(Music classicalMusic, Music rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic(){
-        System.out.println(classicalMusic.getSong() +" "+ rockMusic.getSong());
+        for(Music music: musicList){
+            System.out.println(music.getSong());
+        }
     }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -1,18 +1,15 @@
 package ru.alishev.springcourse;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-                SpringConfig.class
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
         );
 
-       MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-       musicPlayer.playMusic();
-       System.out.println(musicPlayer.getName());
-       System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
 
         context.close();
     }
