6bb13c0d31c204dc784e9967f3b149c8d684ee98
Fataev Farhod <fataev_f@mail.ru>
2020-10-15 20:06:16 +0300

    Factory method

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package ru.alishev.springcourse;
 
 public class ClassicalMusic implements Music {
+
+    //паттерн фабричный метод, мы должны обьявить конструктор приватным и создавать обьект
+
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic(){   //метод который создает новый обьект
+        return new ClassicalMusic();
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
