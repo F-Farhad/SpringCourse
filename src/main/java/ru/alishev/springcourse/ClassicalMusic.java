bc22344c0b56645badd9b6f4abcbeafcf8d55aec
Fataev Farhod <fataev_f@mail.ru>
2020-10-15 19:53:26 +0300

    Init - Destroy method

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -5,4 +5,12 @@ public class ClassicalMusic implements Music {
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
+    public void doMyInit(){
+        System.out.println("Do my Init");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Do my destroy");
+    }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -8,22 +8,8 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-
-        System.out.println(musicPlayer == musicPlayer1);
-        System.out.println(musicPlayer.getVolume() +" musicPlayer");
-        System.out.println(musicPlayer1.getVolume()+" musicPlayer1");
-
-        System.out.println(musicPlayer.getName());
-        musicPlayer.setVolume(10);
-
-        System.out.println(musicPlayer == musicPlayer1);
-        System.out.println(musicPlayer.getVolume() +" musicPlayer");
-        System.out.println(musicPlayer1.getVolume()+" musicPlayer1");
-
-
-        musicPlayer.playMusic();
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
 
         context.close();
