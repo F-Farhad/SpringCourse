8e6ccede35881fa9dc8dda2c20bc273d533bb225
Fataev Farhod <fataev_f@mail.ru>
2020-10-17 16:59:26 +0300

    Configuration with Java class

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -2,11 +2,34 @@ package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     private Music classicalMusic;
+
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Autowired
     @Qualifier("classicalMusic")
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -1,15 +1,18 @@
 package ru.alishev.springcourse;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
 
-       ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
-        System.out.println(classicalMusic.getSong());
+       MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+       musicPlayer.playMusic();
+       System.out.println(musicPlayer.getName());
+       System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
